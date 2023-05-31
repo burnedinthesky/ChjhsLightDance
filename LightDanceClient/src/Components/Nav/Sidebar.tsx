9c407c29d3941901cbabb28d370f5b08cd24db78
burnedinthesky <andrew.mutien@gmail.com>
2023-06-01 07:54:05 +0800

    Update Tauri App Icon

--- LightDanceClient/src/Components/Nav/Sidebar.tsx
+++ LightDanceClient/src/Components/Nav/Sidebar.tsx
@@ -8,7 +8,7 @@ interface SidebarProps {
 const Sidebar = ({ setAppMode }: SidebarProps) => {
     return (
         <div className="h-screen w-16 fixed top-0 left-0 py-10 flex flex-col gap-7 items-center border-r border-r-slate-400">
-            <h2 className="text-blue-800 font-bold text-xl font-jbmono">CH</h2>
+            <h2 className="text-blue-800 font-bold text-xl font-jbmono">CS</h2>
             <ActionIcon
                 size={28}
                 onClick={() => {
